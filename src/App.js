6bd2e079a137dbdf2e47c8c055fa967cc387fa3d
Casey Kriutzfield <casey.kriutzfield@live.com>
2021-03-18 09:53:17 -0400

    You know the drill

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import VendorSelect from './VendorSelect'
+import { Layout } from 'antd';
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="App-header">
+      <Header style={{color: 'white' }}>Casey's Vendor Autocomplete Demo</Header>
+      <Content>
+        <div className="site-layout-content">
+          <VendorSelect />
+        </div>
+      </Content>
+      <Footer>Footer</Footer>
+    </Layout>
+
+    
   );
 }
 
---
+++ src/VendorSelect.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { AutoComplete } from 'antd';
+
+const Complete = () => {
+    const [value, setValue] = useState('');
+    const [options, setOptions] = useState([]);
+    
+    const onSearch = (searchText) => {
+        getData(searchText);
+    };
+    
+    const onSelect = (data) => {
+        console.log('onSelect', data);
+    };
+    
+    const onChange = (data) => {
+        setValue(data);
+    };
+
+    const getData = (data) => {
+        fetch('https://casey-autocomplete-poc.azurewebsites.net/SimpleAutoComplete?highlights=false&fuzzzy=true&term=' + data)
+        .then(res => {
+            if(res.status >= 400) {
+                throw new Error("Somehow this is Caseys fault.");
+            }
+            return res.json();
+        })
+        .then(data => {
+            console.log(data);
+            setOptions(data);
+        });
+    };
+
+    return (
+        <AutoComplete options={options} style={{ width: 300, }} onSelect={onSelect} onSearch={onSearch} placeholder="input here" />
+    )
+};
+
+export default Complete
